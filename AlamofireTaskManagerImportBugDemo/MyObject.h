c56f001214cf59f6918bdefca24808c5f8ba6b0c
Ada Turner <ada.l.turner@gmail.com>
2016-09-09 17:46:25 -0600

    Initial commit (builds ok)

---
+++ AlamofireTaskManagerImportBugDemo/MyObject.h
@@ -0,0 +1,13 @@
+//
+//  MyObject.h
+//  AlamofireTaskManagerImportBugDemo
+//
+//  Created by Ada Turner on 9/9/16.
+//  Copyright © 2016 ADA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyObject : NSObject
+
+@end
---
+++ AlamofireTaskManagerImportBugDemo/MyObject.m
@@ -0,0 +1,13 @@
+//
+//  MyObject.m
+//  AlamofireTaskManagerImportBugDemo
+//
+//  Created by Ada Turner on 9/9/16.
+//  Copyright © 2016 ADA. All rights reserved.
+//
+
+#import "MyObject.h"
+
+@implementation MyObject
+
+@end
